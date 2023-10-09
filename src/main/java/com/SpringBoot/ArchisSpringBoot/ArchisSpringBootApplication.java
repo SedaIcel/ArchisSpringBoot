3a245ef1be23cd060d9e5fc83f7729afb41a0dc0
Seda Icel <sedaozturkicel@gmail.com>
2023-10-09 15:06:47 +0300

    Initial commit

---
+++ src/main/java/com/SpringBoot/ArchisSpringBoot/ArchisSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ArchisSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchisSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchisSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBoot/ArchisSpringBoot/ArchisSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBoot.ArchisSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchisSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
