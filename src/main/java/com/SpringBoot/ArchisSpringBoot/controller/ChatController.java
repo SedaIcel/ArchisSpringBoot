f352adb6a093abd56b654917ea99c9168925123c
Seda Icel <sedaozturkicel@gmail.com>
2023-10-10 00:33:24 +0300

    [SE-5] - Created Chat Controller-Service Class

---
+++ src/main/java/com/SpringBoot/ArchisSpringBoot/controller/ChatController.java
@@ -0,0 +1,22 @@
+package com.SpringBoot.ArchisSpringBoot.controller;
+
+import com.SpringBoot.ArchisSpringBoot.service.ChatService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat")
+public class ChatController {
+    private final ChatService chatService; //dependency injection
+
+    public ChatController(ChatService chatService) {
+        this.chatService = chatService;
+    }
+
+    @PostMapping("/prompt")
+    public String sendMessage(@RequestBody String message){
+        return chatService.sendMessage(message);
+    }
+}
